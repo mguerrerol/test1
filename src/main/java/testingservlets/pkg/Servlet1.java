b66d71d78db7e182bcfd1bc576a2822694968137
MARCO <mguerrerol>
2021-09-12 00:01:36 -0500

    subida primaria de archivos a Github

---
+++ src/main/java/testingservlets/pkg/Servlet1.java
@@ -0,0 +1,53 @@
+package testingservlets.pkg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet(description = "servlet for testing purposes", urlPatterns = { "/Servlet1" })
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name = request.getParameter("nameuser");
+		String ced = request.getParameter("ced");
+		PrintWriter writer = response.getWriter();
+		if( name != null && ced != null) {
+			writer.println("Bienvenido "+ name +" a este primer JSP con Servlet");
+		}
+		else
+			writer.println("Error: Nombre o Cédula faltante!");
+		writer.close();
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Test Servlets and GitHub Repository</title>
+</head>
+<body>
+<h1>BIENVENIDOS AL MUNDO DEL JSP</h1>
+ <br>
+ <form action="./Servlet1" method="get">
+ FORMULARIO <br />
+ <table>
+ <tr>
+ <td><label>
+Teclee su nombre:&nbsp;</label></td>
+<td>
+<input type="text" name="nameuser" id="nameuser"
+size="20" maxlength="50" />
+</td></tr>
+<tr><td><label>
+Teclee su cédula:&nbsp;</label></td>
+<td>
+<input type="text" name="ced" id="ced"
+size="20" maxlength="50" />
+</td></tr>
+<tr><td>
+<input type="submit" id="send" name="send"
+value="Aceptar" />
+</td><td>
+<input type="reset" id="del" value="Borrar" />
+</td></tr>
+</table>
+</form>
+<h2> Hoy es:
+<%= new java.util.Date() %>
+</h2>
+ <%
+java.util.Calendar ahora = java.util.Calendar.getInstance();
+int hora = ahora.get(java.util.Calendar.HOUR_OF_DAY); 
+%>
+</body>
+</html>
